f9e99240e1d83b693eb031b2bb6f5419b01346e3
kirbopoyo21 <140222522+kirbopoyo21@users.noreply.github.com>
2025-07-21 09:07:33 +0000

    botoninicio

--- progreso.js
+++ progreso.js
@@ -1,13 +1,17 @@
-document.getElementById('btn-cursos').onclick = function() {
+document.getElementById('btn-cursos').onclick = function () {
   document.getElementById('cursos-panel').style.display = '';
   document.getElementById('avance-panel').style.display = 'none';
   this.classList.add('activo');
   document.getElementById('btn-avance').classList.remove('activo');
 };
 
-document.getElementById('btn-avance').onclick = function() {
+document.getElementById('btn-avance').onclick = function () {
   document.getElementById('cursos-panel').style.display = 'none';
   document.getElementById('avance-panel').style.display = '';
   this.classList.add('activo');
   document.getElementById('btn-cursos').classList.remove('activo');
 };
+
+document.getElementById("btn-inicio").addEventListener("click", function () {
+  window.location.href = "index.html";
+});
